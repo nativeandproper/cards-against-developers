a46b3ce8d395f0a54155fc441ea358080fc188d0
Carl Bernardo <carlbernrdo@gmail.com>
2018-10-03 22:13:56 -0400

    style: style 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,10 +5,11 @@ import "../styles/NotFound.css";
 const NotFound = ({ location }) => (
   <div className="not-found">
     <div className="not-found-center">
+      <h2>404</h2>
       <div className="not-found-icon" />
-      <h1>Man on Fire!</h1>
+      <h2>Houston, we have a problem.</h2>
       <h2>
-        URL <code> {location.pathname}</code> Not Found.
+        <code> {location.pathname}</code> is not a valid fuel source.
       </h2>
     </div>
   </div>
