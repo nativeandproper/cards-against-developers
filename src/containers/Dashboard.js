51109d4e6aa6436140f45ca2d34f15b94bfae28c
melindabernrdo <melindabernrdo@gmail.com>
2018-10-03 17:29:59 -0700

    adds logout button

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,20 +1,36 @@
 import React from "react";
 
+import apiClient from "../lib/apiClient";
+
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
+    this.state = {
+      logoutError: ""
+    };
   }
 
-  handleChange = (key, value) => {
-    this.setState({
-      [key]: value
-    });
+  onLogout = () => {
+    apiClient("POST", "/logout")
+      .then(res => {
+        this.props.history.push("/login", null);
+      })
+      .catch(err => {
+        err.text().then(errorMsg => {
+          this.setState({
+            logoutError: errorMsg
+          });
+        });
+      });
   };
+
+  // TODO: add error handling from logout
   render() {
     return (
       <div className="dashboard">
+        <button className="nav-button" onClick={this.onLogout}>
+          Log Out
+        </button>
         <h1>Dashboard</h1>
       </div>
     );
