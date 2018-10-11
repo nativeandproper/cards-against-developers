361dc68eaebb5b9a9ec14c8e3389cde12cf05ca3
melindabernrdo <melindabernrdo@gmail.com>
2018-10-10 17:03:41 -0700

    feat: set cah-token in local storage

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,11 +1,11 @@
 import env from "../config/development";
 
 const defaultHeaders = {
-  Accept: "application/json",
+  // Accept: "application/json",
   "Content-Type": "application/json"
 };
 
-export default function api(method, url, data) {
+export default function api(method, url, data, opts = null) {
   return fetch(`${env.API_ROOT}${url}`, {
     body: JSON.stringify(data),
     // TODO: use session token auth for user requests and api token for api requests
@@ -13,6 +13,11 @@ export default function api(method, url, data) {
     headers: defaultHeaders,
     method: method.toUpperCase()
   }).then(res => {
+    // return full response
+    if (opts.fullRes && res.ok) {
+      return res;
+    }
+
     const contentType = res.headers.get("content-type");
     if (contentType && res.ok) {
       switch (contentType) {
